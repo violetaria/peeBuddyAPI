3563627f3183cde231360d9d216bc5402d31c270
violetaria <terri.chu@outlook.com>
2016-05-23 20:09:27 -0400

    update to use api in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    render json: { errors: "Object not found: #{error.message}" }, status: :not_found
+  end
 end
